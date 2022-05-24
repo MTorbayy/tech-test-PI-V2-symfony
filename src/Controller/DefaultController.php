78d0eebcb4e8045d9bfcc4421220b5e7556cb6a6
Marie <marie.torbay@gmail.com>
2022-05-24 13:48:32 +0200

    finished project

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,9 +7,11 @@ use App\Repository\RoomsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends AbstractController
-{
+{   
+    
     /**
      * @Route("/rooms/{id}", name="roomsPerMonth", requirements={"id"="^([1-9]|1[012])$"}, methods={"GET"})
      */
@@ -17,7 +19,28 @@ class DefaultController extends AbstractController
     public function roomsPerMonth(RoomsRepository $roomsRepository, $id): Response
     {
         $roomsByMonth = $roomsRepository->findByMonth($id);
+        
+        $jsonPrepare = [];
 
-        dump($roomsByMonth);die;
+
+        foreach($roomsByMonth as $room) {
+            $roomPrepare = ['stay_date' => $room->getStayDate()->format('Y-m-d'), 
+                            'room_nights' => (string) $room->getRoomNights(), 
+                            'room_revenues' => (string) $room->getRoomRevenues()];
+
+            
+            array_push($jsonPrepare, (object) $roomPrepare);
+            
+        }
+
+        function sendJSON($info) {
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json");
+            echo json_encode($info);
+        }
+
+        sendJSON($jsonPrepare);
+        die;
+
     }
 }
